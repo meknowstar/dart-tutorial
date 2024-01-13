51772035b6aab93af3a52091d01fbb8747bb94c7
seongminho <smiku0917@gmail.com>
2024-01-14 03:18:59 +0900

    var keyword and type notation

---
+++ 02_var_keyword.dart
@@ -0,0 +1,16 @@
+// dart style guide.
+// usually local variable use var keyword.
+// when use class variable or property use type.
+
+void main() {
+  var name = "robot1";
+  name = 'robot2';
+  print(name);
+}
+
+
+// void main() {
+//   String name = "robot1";
+//   name = 'robot2';
+//   print(name);
+// }
