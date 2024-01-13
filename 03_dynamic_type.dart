fd971864ae7dcd3b03c255cba4244678ec65a4c1
seongminho <smiku0917@gmail.com>
2024-01-14 03:23:22 +0900

    dynamic type

---
+++ 03_dynamic_type.dart
@@ -0,0 +1,21 @@
+// it doesnt recommend to use, but sometimes very useful.
+// you need to use when you really x2 need.
+
+// void main() {
+//   // Type: dynamic - you can also specify type of the variable.
+//   // var name;
+//   dynamic name;
+//   name = 'nico';
+//   name = 12;
+//   name = true;
+// }
+
+void main() {
+  dynamic name;
+  if (name is String) {
+    // name. alot of properties...
+  }
+  if (name is int) {
+    // name. alot of properties...
+  }
+}
